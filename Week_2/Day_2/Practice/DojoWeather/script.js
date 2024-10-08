da46b3c33fb29fdef89f8d092a78d322b1a5a3e3
YassinBn02 <yassinbn02@gmail.com>
2024-10-08 19:28:25 +0200

    Dojo weather

---
+++ Week_2/Day_2/Practice/DojoWeather/script.js
@@ -0,0 +1,20 @@
+function alertt(){
+    alert("Loading weather report...")
+}
+
+function removee(id){
+    document.getElementById(id).remove();
+}
+
+function tempi(id){
+    var e=document.getElementById(id);
+    e.innerText=Math.floor(e.innerText*1.8)
+}
+
+var arr=["red","red2","red3","red4","blue","blue2","blue3","blue4"]
+
+function changeTemp(){
+    for(var i=0;i<arr.length;i++){
+        tempi(arr[i])
+    }
+}
