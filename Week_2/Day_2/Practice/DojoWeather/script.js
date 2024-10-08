ee90bce2e2101ef4d8ab13fe3c9e139cb1b5ca95
YassinBn02 <yassinbn02@gmail.com>
2024-10-08 19:35:32 +0200

    update Dojo Weather

--- Week_2/Day_2/Practice/DojoWeather/script.js
+++ Week_2/Day_2/Practice/DojoWeather/script.js
@@ -8,7 +8,7 @@ function removee(id){
 
 function tempi(id){
     var e=document.getElementById(id);
-    e.innerText=Math.floor(e.innerText*1.8)
+    e.innerText=Math.floor(e.innerText*1.8+32)
 }
 
 var arr=["red","red2","red3","red4","blue","blue2","blue3","blue4"]
